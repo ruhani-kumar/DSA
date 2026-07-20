d4712921fc0800a77b8223ae147992e4cbd1bf3a
ruhani-kumar <rk.learn05@gmail.com>
2026-07-20 10:03:31 +0530

    Time: 80 ms (69.20%) | Memory: 32.1 MB (33.39%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,6 +1,6 @@
 class MinStack:
 
-    def __init__(self):
+    '''def __init__(self):
         self.st = []
         self.min = []
 
@@ -10,8 +10,8 @@ class MinStack:
             self.min.append(value)
         elif value<=self.min[-1]:
             self.min.append(value)
-        '''if len(self.min) == 0 or value <= self.min[-1]:
-            self.min.append(value)'''
+        //if len(self.min) == 0 or value <= self.min[-1]:
+            self.min.append(value)//
 
     def pop(self) -> None:
         top = self.st.pop()
@@ -22,8 +22,27 @@ class MinStack:
         return self.st[-1]
 
     def getMin(self) -> int:
-        return self.min[-1]
+        return self.min[-1]'''
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
 
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if len(self.min_stack) == 0 or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+        if val == self.getMin():
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+        
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
