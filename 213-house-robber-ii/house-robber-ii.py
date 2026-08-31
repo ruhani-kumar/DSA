6cab37a153194552548cf1924dfdfaf6309ff54a
ruhani-kumar <rk.learn05@gmail.com>
2026-08-31 09:37:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (68.92%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
+        
+    def helper(self, nums):
+        r1 = 0
+        r2 = 0
+        for n in nums:
+            temp = max(r1+n, r2)
+            r1 = r2
+            r2 = temp
+        return r2
+        
